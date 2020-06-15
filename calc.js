7db1db8e31010829ca038900d1c72e3c9eec4e24
Samanth N <35089618+SamBro2901@users.noreply.github.com>
2020-06-15 11:27:39 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -5,11 +5,12 @@ function calculate()
     var max_xp_gain_per_message = 25;
 
     //Input from user
-    var desired_level = document.getElementById("desired-level").value;
+    var current_level = document.getElementById("current-level").value;
     var current_xp = document.getElementById("current-xp").value;
-
+    var desired_level = document.getElementById("desired-level").value;
+    
     //Magical math functions
-    var xp_to_desired_level = 5 / 6 * desired_level * (2 * desired_level * desired_level + 27 * desired_level + 91);
+    var xp_to_desired_level = 5 * (Math.pow(current_level, 2)) + 50*current_level + 100;
     var xp_needed = xp_to_desired_level - current_xp;
 
     //Minimum, average, and maximum messages needed to send in order to reach specified level
