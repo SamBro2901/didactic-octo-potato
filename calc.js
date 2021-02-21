4af42d6c72908e23e388eba6e1f068688cebc7e6
Samanth N <35089618+SamBro2901@users.noreply.github.com>
2021-02-21 20:24:27 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -5,23 +5,21 @@ function calculate()
     var max_xp_gain_per_message = 25;
 
     //Input from user
-    var current_level = document.getElementById("current-level").value;
+    var x = document.getElementById("current-level").value;
     var current_xp = document.getElementById("current-xp").value;
-    var desired_level = document.getElementById("desired-level").value;
+    var n = document.getElementById("desired-level").value;
     
     //Magical math functions
-    var xp = 0;
-    for(i = current_level; i<desired_level; i++){
-        xp = xp + 5*Math.pow(i, 2) + 50*i + 100;
-    }
-    var xp_needed = xp-current_xp;
+    var xp1 = 5*(x*(x+1)*(2*x+1)/6)+50*(x*(x+1)/2)+100(x+1);
+    var xp2 =  5*(n*(n+1)*(2*n+1)/6)+50*(n*(n+1)/2)+100(n+1);
+    var xp_needed = xp2-xp1-current_xp;
 
     //Minimum, average, and maximum messages needed to send in order to reach specified level
     var min_messages_needed_to_send = Math.ceil(xp_needed / max_xp_gain_per_message);
     var avg_messages_needed_to_send = Math.ceil(xp_needed / ((min_xp_gain_per_message + max_xp_gain_per_message) / 2));
     var max_messages_needed_to_send = Math.ceil(xp_needed / min_xp_gain_per_message);
 
-    if(xp_needed<0 || current_level<0 || desired_level<0 || current_xp<0){
+    if(xp_needed<0 || x<0 || n<0 || current_xp<0 || x>n){
         min_messages_needed_to_send = "_";
         avg_messages_needed_to_send = "_";
         max_messages_needed_to_send = "_";
